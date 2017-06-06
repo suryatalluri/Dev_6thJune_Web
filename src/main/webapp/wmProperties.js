fb886d140afdcd68021b5c1522e293044e0d5b13
root <root@a8d1f089f3e0>
2017-06-06 05:35:29 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Dev 6thJune Web",
+  "homePage" : "Main",
+  "name" : "Dev_6thJune_Web",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
